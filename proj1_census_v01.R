ab43bd4185bffac8690b484733f6c256cd3ad0d4
AliTabesh <a_t110@ymail.com>
2020-11-19 04:24:35 +0100

    Task 3.2.2 added which is a dataframe consists of variances for each region and whole data in its last row.

--- proj1_census_v01.R
+++ proj1_census_v01.R
@@ -5,6 +5,9 @@ library(DataExplorer)
 library(stats)
 library(gridExtra)
 
+#library(lemon) ## to print dataframes in a table format in R markdown (run both lines)
+#knit_print.data.frame <- lemon_print   ##(run both lines)
+
 #reading the data
 censusData <- read.csv("census_2019_1999.csv")
 #check the data dimentions and features
@@ -61,8 +64,8 @@ corrplot.mixed(cor_numVar, tl.col="black", tl.pos = "lt")
 plot_correlation(dataOf2019[,4:7], ggtheme = ggpubr::theme_pubr(base_size = 10),
                                type = "c", cor_args = list(use = "complete.obs"))
 #-------------------------------------------------------------------TASK 3------
-#Scatter Plot for showing the variance BETWEEN the regions******************************
-betweenRegions_plots <-list()
+#Scatter Plot for showing the variance BETWEEN the regions**********TASK 3.1****
+betweenRegions_plots <-list()  ##list to store plots of task 3.1
 
 betweenRegions_plots[[1]] <- ggplot(data = dataOf2019, aes(x = region, y = total.fertility.rate.per.woman, colour = region)) +
     geom_boxplot() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
@@ -79,10 +82,11 @@ betweenRegions_plots[[4]] <- ggplot(data = dataOf2019, aes(x = region, y = life.
 grid.arrange(grobs = betweenRegions_plots, nrow = 2)
 
 
-#Plot for showing the variance WITHIN each regions******************************
+#Plot for showing the variance WITHIN each regions******************TASK 3.2.1**
+
+uniqueRegions <- unique(dataOf2019$region)
 
 fertility_scatter_plots <- list()
-uniqueRegions <- unique(dataOf2019$region)
 for (i in 1:length(uniqueRegions)) {
 
     avrageInRegion <- mean(dataOf2019[["total.fertility.rate.per.woman"]][dataOf2019$region ==uniqueRegions[i]])
@@ -98,7 +102,6 @@ for (i in 1:length(uniqueRegions)) {
 grid.arrange(grobs = fertility_scatter_plots, nrow = 2)
 
 bothSexesLE_scatter_plots <- list()
-uniqueRegions <- unique(dataOf2019$region)
 for (i in 1:length(uniqueRegions)) {
     
     avrageInRegion <- mean(dataOf2019[["life.expectancy.both.sexes"]][dataOf2019$region ==uniqueRegions[i]])
@@ -114,7 +117,6 @@ for (i in 1:length(uniqueRegions)) {
 grid.arrange(grobs = bothSexesLE_scatter_plots, nrow = 2)
 
 maleLE_scatter_plots <- list()
-uniqueRegions <- unique(dataOf2019$region)
 for (i in 1:length(uniqueRegions)) {
     
     avrageInRegion <- mean(dataOf2019[["life.expectancy.male"]][dataOf2019$region ==uniqueRegions[i]])
@@ -130,7 +132,6 @@ for (i in 1:length(uniqueRegions)) {
 grid.arrange(grobs = maleLE_scatter_plots, nrow = 2)
 
 femaleLE_scatter_plots <- list()
-uniqueRegions <- unique(dataOf2019$region)
 for (i in 1:length(uniqueRegions)) {
     
     avrageInRegion <- mean(dataOf2019[["life.expectancy.female"]][dataOf2019$region ==uniqueRegions[i]])
@@ -145,4 +146,32 @@ for (i in 1:length(uniqueRegions)) {
 }
 grid.arrange(grobs = femaleLE_scatter_plots, nrow = 2)
 
+
+
+#calculate variances of each column for the data in ****************TASK 3.2.2**
+# each region and put all Vars in a dataframe called "varianceInRegions" 
+
+varianceInRegions <- data.frame(uniqueRegions)
+
+for (i in 1:length(uniqueRegions)) {
+    
+    for (j in 4:7) {
+        varianceInRegions[i,j-2] <- var(dataOf2019[dataOf2019$region==uniqueRegions[i] , j])
+        
+    }
+    
+}
+colnames(varianceInRegions)[2:5] <- colnames(dataOf2019)[4:7]
+
+# Now we add the Variance of each column for whole data and save it in last row 
+# and name the last row "World"
+varianceInRegions[22, 1] <- "World"
+varianceInRegions[22, 2] <- var(dataOf2019[ , 4])
+varianceInRegions[22, 3] <- var(dataOf2019[ , 5])
+varianceInRegions[22, 4] <- var(dataOf2019[ , 6])
+varianceInRegions[22, 5] <- var(dataOf2019[ , 7])
+
+varianceInRegions
+
+#ggplot(varianceInRegions, aes(x = uniqueRegions, y = total.fertility.rate.per.woman)) + geom_point()
 
